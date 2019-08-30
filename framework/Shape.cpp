1da39d1279a2e1d442190aab23b38a77242544d2
Simon M <Simon M>
2019-08-30 19:57:11 +0200

    transform - funtions ready (and I hope ok), --last task to do: transformRay-function not ok/ not ready implement in rendering ray / hitpoint intersection ;

--- framework/Shape.cpp
+++ framework/Shape.cpp
@@ -34,10 +34,10 @@ std::string Shape::get_name(){
 void Shape::scale(float x,float y,float z){
 
     glm::mat4 matrix = glm::mat4{
-    glm::vec4{x, 0.0f, 0.0f, 0.0f}, 
-    glm::vec4{0.0f, y, 0.0f, 0.0f},
-    glm::vec4{0.0f, 0.0f, z, 0.0f}, 
-    glm::vec4{0.0f, 0.0f, 0.0f, 1.0f}};
+        glm::vec4{x, 0.0f, 0.0f, 0.0f}, 
+        glm::vec4{0.0f, y, 0.0f, 0.0f},
+        glm::vec4{0.0f, 0.0f, z, 0.0f}, 
+        glm::vec4{0.0f, 0.0f, 0.0f, 1.0f}};
 
     glm::inverse(glm::transpose(matrix)); 
 
@@ -50,10 +50,65 @@ void Shape::scale(float x,float y,float z){
 
 void Shape::translate(float x,float y,float z){
 
+    glm::mat4 matrix = glm::mat4{
+        glm::vec4{1.0f, 0.0f, 0.0f, 0.0f}, 
+        glm::vec4{0.0f, 1.0f, 0.0f, 0.0f},
+        glm::vec4{0.0f, 0.0f, 1.0f, 0.0f}, 
+        glm::vec4{x, y, z, 1.0f}};
+
+    glm::inverse(glm::transpose(matrix)); 
+
+    world_transformation_ *= matrix;
+    world_transformation_inv_ = glm::inverse(world_transformation_);
+
     std::cout << "hello from translate Shape.cpp \n";
 }
 
 void Shape::rotate(float angle,float x,float y,float z){
+
+    if(x == 1){
+        glm::mat4 matrix = glm::mat4{
+            glm::vec4{1.0f, 0.0f, 0.0f, 0.0f},
+            glm::vec4{0.0f, cos(angle), -sin(angle), 0.0f},
+            glm::vec4{0.0f,sin(angle),cos(angle),0.0f},
+            glm::vec4{0.0f,0.0f,0.0f,1.0f}      
+        };
+    
+        glm::inverse(glm::transpose(matrix)); 
+
+        world_transformation_ *= matrix;
+        world_transformation_inv_ = glm::inverse(world_transformation_);
+
+    }
+    
+    if(y == 1){
+        glm::mat4 matrix = glm::mat4{
+            glm::vec4{sin(angle),0.0f,cos(angle),0.0f},
+            glm::vec4{0.0f, 1.0f, 0.0f, 0.0f},
+            glm::vec4{cos(angle), 0.0f, -sin(angle), 0.0f},
+            glm::vec4{0.0f,0.0f,0.0f,1.0f}      
+        };
+
+        glm::inverse(glm::transpose(matrix)); 
+
+        world_transformation_ *= matrix;
+        world_transformation_inv_ = glm::inverse(world_transformation_);
+    }
+          
+    if(z == 1){
+           
+        glm::mat4 matrix = glm::mat4{
+            glm::vec4{cos(angle), -sin(angle), 0.0f, 0.0f},
+            glm::vec4{sin(angle), cos(angle), 0.0f, 0.0f},
+            glm::vec4{0.0f,0.0f,1.0f,0.0f},
+            glm::vec4{0.0f,0.0f,0.0f,1.0f}      
+        };
+
+        glm::inverse(glm::transpose(matrix)); 
+
+        world_transformation_ *= matrix;
+        world_transformation_inv_ = glm::inverse(world_transformation_);
+    }
 
     std::cout << "hello from rotate Shape.cpp \n";
 
